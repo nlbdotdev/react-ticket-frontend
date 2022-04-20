6fa57243b97ee3b206925b87f6457cd8606cef2f
ReflextionsDev <reflextionsdev@gmail.com>
2022-04-20 15:43:04 -0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-
+import Navbar from './components/Navbar';
 import React, { Component } from 'react'
+import { Outlet } from 'react-router-dom';
 
 export class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div>
+        <h1>React Router</h1>
+        <Navbar />
+        <div>
+          <Outlet />
+        </div>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home";
+import NewTask from "./pages/NewTask";
+import NotFound from "./pages/NotFound"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="newtask" element={<NewTask />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
