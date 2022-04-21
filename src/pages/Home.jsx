0def6f1656aec3b8ed84e296d35cf793939f97b4
ReflextionsDev <reflextionsdev@gmail.com>
2022-04-21 14:39:46 -0400

    install reduxjs/toolkit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import Task from '../components/Task'
+
+const tasks = [
+  {
+    task: 'task one',
+    status: 'completed',
+    priority: 'high'
+  },
+  {
+    task: 'task two',
+    status: 'incompleted',
+    priority: 'medium'
+  },
+  {
+    task: 'task three',
+    status: 'completed',
+    priority: 'low'
+  },
+  {
+    task: 'task four',
+    status: 'incompleted',
+    priority: 'high'
+  },
+]
 
 export default function Home() {
   return (
-    <div>Home</div>
+
+    <div>Home
+
+      {tasks.map(() => {
+        return <Task />
+      })}
+
+    </div>
+
   )
 }
