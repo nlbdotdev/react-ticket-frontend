2644b6da02799709b40799ba8ee20cfd8ac427ec
ReflextionsDev <reflextionsdev@gmail.com>
2022-04-25 15:51:00 -0400

    postTask thunk

--- server/routes/Tasks/controller/taskController.js
+++ server/routes/Tasks/controller/taskController.js
@@ -2,7 +2,9 @@ const Task = require("../model/Task");
 
 const createTask = async (req, res) => {
     try {
+        console.log(req.body)
         const { uid, title, status, priority } = req.body
+   
 
         let newTask = new Task({
             uid: uid,
